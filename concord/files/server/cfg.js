ae7a8ebc3e2ac3aff00f13828400d3a83b75f753
Al Wiesner <al.wiesner@starburstdata.com>
2020-06-27 15:00:45 -0400

    Add OIDC support

---
+++ concord/files/server/cfg.js
@@ -0,0 +1,15 @@
+// environment specific data
+window.concord = {
+    documentationSite: 'https://concord.walmartlabs.com',
+    topBar: {
+        systemLinks: [
+            {
+                text: 'GitHub',
+                url: 'https://github.com/walmartlabs/concord',
+                icon: 'github'
+            }
+        ]
+    },
+    loginUrl: '/api/service/oidc/auth',
+    logoutUrl: '/api/service/oidc/logout'
+};
